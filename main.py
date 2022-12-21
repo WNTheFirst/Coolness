9a3b30ea2cbb0792fe9b6e7f07022e6f5a0e4380
wen wine <120588866+WNTheFirst@users.noreply.github.com>
2022-12-21 17:16:37 +0000

    made a calculator

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-#ahem*
-print("loki sucks at video games")
+no1 = float(input("add your first number: "))
+op = input("enter operator: ")
+no2 = float(input("add your second number: "))
+
+if op == "+":
+  print(no1 + no2)
+elif op == "-":
+  print(no1 - no2)
+elif op == "/":
+  print(no1 / no2)
+elif op == "*":
+  print(no1 * no2)
+
+else:
+  print("Invalid Operator.")
